c2c56d31a0171568d26d9d1a650c86159aa6f423
algarfer <algarferx@proton.me>
2024-09-16 13:15:02 +0200

    Añadida plantilla

---
+++ template.ino
@@ -0,0 +1,18 @@
+
+/**
+ * EjercicioX.X - Grupo L2-A5
+ * 
+ * Alvaro Garcia Fernandez - UO289295
+ * Daniel Andina Pailos - UO287791
+ * Enol Rodriguez Hevia - UO287935
+ */
+
+
+void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+	
+}
